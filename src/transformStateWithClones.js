088c8a63c601e15cd76eb81b65521ccaae2a4bd4
michelam <michelammilian@outlook.com>
2025-06-20 14:32:57 -0300

    Resolução refinada da tarefa após sugestão de correções da IA Mate

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,52 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const chainActions = [];
+  let keys = [];
+  let values = [];
+
+  for (const action of actions) {
+    let objectAction = {};
+
+    switch (action.type) {
+      case 'addProperties':
+        (() => {
+          return chainActions.length === 0
+            ? (objectAction = { ...state })
+            : (objectAction = { ...chainActions[chainActions.length - 1] });
+        })();
+
+        keys = Object.keys(action.extraData);
+        values = Object.values(action.extraData);
+
+        for (let i = 0; i < keys.length; i++) {
+          objectAction[keys[i]] = values[i];
+        }
+
+        break;
+
+      case 'removeProperties':
+        (() => {
+          return chainActions.length === 0
+            ? (objectAction = { ...state })
+            : (objectAction = { ...chainActions[chainActions.length - 1] });
+        })();
+
+        for (let i = 0; i < keys.length; i++) {
+          delete objectAction[keys[i]];
+        }
+
+        break;
+
+      case 'clear':
+        objectAction = {};
+        break;
+    }
+
+    chainActions.push(objectAction);
+  }
+
+  return chainActions;
 }
 
 module.exports = transformStateWithClones;
